5ee948a52dd3de0963468307adea1ee3bbe69696
Tompalski <piotr.tompalski@nrcan-rncan.gc.ca>
2022-06-15 09:26:04 -0700

    dataset created

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Canadian tree species data
+#'
+#' A dataset containing Canadian tree species names (English, French, Scientific) and
+#' species codes used at the national level (NFI) and provincial/territorial.
+#'
+#' @format A data frame with 367 rows and 20 variables:
+#' \describe{
+#'   \item{SPP_CODE}{NFI species code}
+#'   \item{CommonNameEnglish}{Common species name in English}
+#'   \item{CommonNameFrench}{Common species name in French}
+#'   \item{ScientificName}{Scientific species name (Latin)}
+#'   \item{Genus}{Four letter genus code}
+#'   \item{Species}{Three letter species code}
+#'   \item{Var}{Three letter variety code}
+#'   \item{ab_code, bc_code, nb_code, nt_code, on_code, ns_code, sk_code, yt_code, pe_code, qc_code, mb_code, nl_code}{Provincial/Territorial species codes}
+#'   \item{canfi_code}{Numeric species code}
+#' }
+#' @source
+#' https://nfi.nfis.org/en/documentation
+#'
+#' https://github.com/CASFRI/CASFRI/blob/master/translation/tables/species_code_mapping.csv
+
+"CanadianTreeSpeciesData"
---
+++ data-raw/CanadianTreeSpeciesData.R
@@ -0,0 +1,76 @@
+## code to prepare `CanadianTreeSpeciesData` dataset goes here
+
+library(tidyverse)
+library(magrittr)
+
+# download CASFRI species code mapping
+CASFRI_species <- read_csv("https://raw.githubusercontent.com/CASFRI/CASFRI/master/translation/tables/species_code_mapping.csv")
+
+#read NFI species
+NFI_species <- read_csv("data-raw/NFI_species.csv")
+
+#read canfi species codes
+canfi_species <- read_csv("data-raw/canfi_species.csv")
+
+
+### combining datasets
+
+#add casfri code to NFI_species
+NFI_species %<>%
+  mutate(Var2 = if_else(is.na(Var),"###",Var)) %>% #temporary column for Var with "###" instead of NA
+  unite(col = casfri_species_codes, Genus, Species, Var2, sep = "_", remove = F) %>%
+  select(-Var2)
+
+
+#combine NFI and CASFRI species
+S <- CASFRI_species %>% left_join(NFI_species)
+
+#there are more species listed in CASFRI than in NFI:
+S %>% select(casfri_species_codes, scientific_name, Genus, Species, Var, ends_with("codes")) %>%
+  filter(is.na(Genus)) %>%
+  print(n=50)
+
+# for species that are not present in NFI, get genus/species/var from casfri_species_code
+S %<>%
+  separate(casfri_species_codes, into = c("genus2", "species2", "var2"), sep = "_", remove = F) %>%
+  mutate(var2 = if_else(var2 == "###", NA_character_, var2)) %>%
+
+  mutate(Genus = if_else(is.na(Genus), genus2, Genus)) %>%
+  mutate(Species = if_else(is.na(Species), species2, Species)) %>%
+  mutate(Var = if_else(is.na(Var), var2, Var)) %>%
+  mutate(ScientificName = if_else(is.na(ScientificName), scientific_name, ScientificName)) %>%
+  select(-genus2, -species2, -var2)
+
+
+# combine with canfi_species codes
+S %<>% unite(spp, Genus, Species, sep=".", remove = F) %>%
+  left_join(canfi_species) %>%
+  rename(canfi_species_code = canfi_species) %>%
+  select(-spp)
+
+### add NFI-like species code
+S %<>% unite(SPP_CODE, Genus, Species, Var, sep = ".", remove = F, na.rm = T)
+
+
+### cleanup ####
+S %<>%  select(-casfri_species_codes_OLD,
+               -scientific_name,
+               -casfri_species_codes,
+               -Comment,
+               -pc02_species_codes,
+               -pc01_species_codes,
+               -Form,
+               -Type)
+
+
+S %<>% rename_with(.fn = ~str_replace(.x, "_species_codes", "_code"))
+S %<>% rename(canfi_code = canfi_species_code)
+
+S %<>% relocate(SPP_CODE, CommonNameEnglish, CommonNameFrench, ScientificName, Genus, Species, Var)
+
+### save
+CanadianTreeSpeciesData <- S
+
+write_csv(CanadianTreeSpeciesData, "data-raw/CanadianTreeSpeciesData.csv")
+
+usethis::use_data(CanadianTreeSpeciesData, overwrite = TRUE)
